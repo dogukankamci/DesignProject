f96f422ebab19acf7b30bcca929fa9e151c8251b
Doğukan Kamçı <dogukankamci@gmail.com>
2023-01-06 20:46:48 +0300

    Upgrades
    Background images are changed. Option menu is added.

--- app/src/main/java/com/dogukan/hizolcer/SelectOption.java
+++ app/src/main/java/com/dogukan/hizolcer/SelectOption.java
@@ -8,12 +8,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
 
 public class SelectOption extends AppCompatActivity {
         private Button button;
@@ -38,24 +32,6 @@ public class SelectOption extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 openTrackSpeed();
-            }
-        });
-
-
-        FirebaseDatabase database = FirebaseDatabase.getInstance("https://designproject-adec5-default-rtdb.firebaseio.com");
-        DatabaseReference myRef = database.getReference("Anlık Hız");
-
-
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                int value = dataSnapshot.getValue(int.class);
-                Log.i("Hız","Hedef kişinin anlık hızı: " +value);
-            }
-            @Override
-            public void onCancelled(DatabaseError error) {
-                Log.i("Hız", "Bulut sisteminden hız verisi okunamadı." +
-                        "Bir sorun oluştu.", error.toException());
             }
         });
 
--- app/src/main/java/com/dogukan/hizolcer/TrackSpeed.java
+++ app/src/main/java/com/dogukan/hizolcer/TrackSpeed.java
@@ -4,14 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class TrackSpeed extends AppCompatActivity {
-
+    TextView twspeed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track_speed);
 
+        twspeed = (TextView) findViewById(R.id.textView4);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://designproject-adec5-default-rtdb.firebaseio.com");
+        DatabaseReference myRef = database.getReference("Anlık Hız");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                int value = dataSnapshot.getValue(int.class);
+                Log.i("Hız","Hedef kişinin anlık hızı: " +value);
+                twspeed.setText("Anlık Hız = " +value + " km/h");
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Log.i("Hız", "Bulut sisteminden hız verisi okunamadı." +
+                        "Bir sorun oluştu.", error.toException());
+            }
+        });
 
     }
 }
